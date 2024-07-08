60bc54dd4c82e7ca3915952e629754bb11b74706
TIM <serdus18@yandex.ru>
2024-07-08 14:22:48 +0300

    Creat two new views and urls

---
+++ 1.1-first-project/first_project/app/views.py
@@ -0,0 +1,51 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from datetime import datetime, timezone
+import time
+import os
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    template_name = ''
+    current_time = datetime.fromtimestamp(time.time(), timezone.utc)
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    # по аналогии с `time_view`, напишите код,
+    # который возвращает список файлов в рабочей 
+    # директории
+    # directory/folder path
+    dir_path = r'C:\Users\Михаил\PycharmProjects\dj-homeworks\1.1-first-project\first_project'
+
+    # list to store files
+    res = []
+
+    # Iterate directory
+    for file_path in os.listdir(dir_path):
+        # check if current file_path is a file
+        if os.path.isfile(os.path.join(dir_path, file_path)):
+            # add filename to list
+            res.append(file_path)
+    return HttpResponse(res)
+
+    raise NotImplemented
